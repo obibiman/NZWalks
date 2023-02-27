39150f02d789a64afdf02ee21f0ffd09e8b1ef87
obibiman <obibiman@yahoo.com>
2023-02-26 21:27:28 -0600

    End of Section 2

--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+var connString = builder.Configuration.GetConnectionString("NZWalksConn");
+builder.Services.AddDbContext<NZWalksDbContext>(options => options.UseSqlServer(connString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
